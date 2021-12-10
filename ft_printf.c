0569305a5e7a1ba1856473024dd66a491ee03742
Julia BATORO <jbatoro@e1r8p1.42.fr>
2021-12-10 19:31:24 +0100

    modif

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jbatoro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:13:58 by jbatoro           #+#    #+#             */
-/*   Updated: 2021/12/10 16:57:36 by jbatoro          ###   ########.fr       */
+/*   Updated: 2021/12/10 19:23:46 by jbatoro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "printf.h"
@@ -15,6 +15,10 @@ int ft_printf(const char *s, ...)
 {
 	int i;
 	char *string;
+	int	nb;
+	char c;
+	unsigned int u_nb; 
+	float dec_nb;
 
 	i = 0;
 	va_list args;
@@ -27,29 +31,78 @@ int ft_printf(const char *s, ...)
 			ft_putchar_fd(s[i], 1);
 			i++;
 		}
-		while (s[i]== '%')
+		while (s[i] == '%')
 			i++;
-		if (s[i]  == 's')
-		{
-			while (s[i] && s[i] != '%')
+		/*
+			if (s[i]  == 's')
 			{
-				string = va_arg(args, char *);
-				ft_putstr_fd(string, 1);
-				i++;
+				while (s[i] && s[i] != '%')
+				{
+					string = va_arg(args, char *);
+					ft_putstr_fd(string, 1);
+					i++;
+				}
 			}
+			if (s[i] == 'i')
+			{	
+				nb = 0;
+				while (s[i] && s[i] != '%')
+				{
+					nb = va_arg(args, int);
+					ft_putnbr_fd(nb, 1);
+					i++;
+				}
+			}
+
+			if (s[i] == 'c')
+			{
+				while (s[i] && s[i] != '%')
+				{
+					c = va_arg(args, int);
+					ft_putchar_fd(c, 1);
+					i++;
+				}
+			}
+			if (s[i] == 'u')
+			{
+				while (s[i] && s[i] != '%')
+				{
+					u_nb = va_arg(args, unsigned int);
+					ft_putnbr_fd(u_nb, 1);
+					i++;
+				}
+			} */
+			if (s[i] == 'd')
+			{
+				while (s[i] && s[i] != '%')
+				{
+					dec_nb  = va_arg(args, double);
+					ft_putnbr_fd(dec_nb, 1);
+					i++;
+				}
+			}
+				
 		}
-	}
 	va_end (args);
 	return (i);
 }
 
+#include <stdio.h>
 int main(void)
 {
 	int nb = 20;
 	char *str = "ca va?";
+	char	c = 'Y';
+	unsigned int u_nb = 123456;
+	float dec_nb = 1.5;
 
 	ft_printf("Hello ca va ?\n");
-	ft_printf("%s", str);
-	//ft_printf("%s/%d\n", "Nice", 20);
+//	ft_printf("%s", str);
+//	ft_printf("%c", c);
+//	ft_printf("%u", u_nb);
+//	ft_printf("%p", nb);
+//	ft_printf("%i", nb);
+//	ft_printf("%x", nb);
+	ft_printf("%d", dec_nb);
 	return (0);
 }
